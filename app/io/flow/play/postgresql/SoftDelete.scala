29db8e1873449e1e33bc0ea475f56002ac49a72c
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-08 00:36:12 -0500

    Remove dependency on flow User - use user guid instead

--- app/io/flow/play/postgresql/SoftDelete.scala
+++ app/io/flow/play/postgresql/SoftDelete.scala
@@ -1,6 +1,5 @@
 package io.flow.play.postgresql
 
-import io.flow.user.v0.models.User
 import anorm._
 import play.api.db._
 import play.api.Play.current
@@ -11,35 +10,35 @@ import java.util.UUID
   */
 object SoftDelete {
 
-  def delete(tableName: String, deletedBy: User, guid: String) {
+  def delete(tableName: String, deletedBy: UUID, guid: String) {
     delete(tableName, deletedBy, UUID.fromString(guid))
   }
 
-  def delete(c: java.sql.Connection, tableName: String, deletedBy: User, guid: String) {
+  def delete(c: java.sql.Connection, tableName: String, deletedBy: UUID, guid: String) {
     delete(c, tableName, deletedBy, UUID.fromString(guid))
   }
 
-  def delete(tableName: String, deletedBy: User, guid: UUID): Unit = {
+  def delete(tableName: String, deletedBy: UUID, guid: UUID): Unit = {
     delete(tableName, deletedBy, ("guid", Some("::uuid"), guid.toString))
   }
 
-  def delete(c: java.sql.Connection, tableName: String, deletedBy: User, guid: UUID): Unit = {
+  def delete(c: java.sql.Connection, tableName: String, deletedBy: UUID, guid: UUID): Unit = {
     delete(c, tableName, deletedBy, ("guid", Some("::uuid"), guid.toString))
   }
 
-  def delete(tableName: String, deletedBy: User, field: (String, Option[String], String)) {
+  def delete(tableName: String, deletedBy: UUID, field: (String, Option[String], String)) {
     DB.withConnection { implicit c =>
       delete(c, tableName, deletedBy, field)
     }
   }
 
-  def delete(c: java.sql.Connection, tableName: String, deletedBy: User, field: (String, Option[String], String)) {
+  def delete(c: java.sql.Connection, tableName: String, deletedBy: UUID, field: (String, Option[String], String)) {
     val (name, tpe, value) = field
     val SoftDeleteQuery = s"""
       update %s set deleted_by_guid = {deleted_by_guid}::uuid, deleted_at = now() where ${name} = {${name}}${tpe.getOrElse("")} and deleted_at is null
                           """
     DB.withConnection { implicit c =>
-      SQL(SoftDeleteQuery.format(tableName)).on('deleted_by_guid -> deletedBy.guid, name -> value).execute()
+      SQL(SoftDeleteQuery.format(tableName)).on('deleted_by_guid -> deletedBy, name -> value).execute()
     }
   }
 
