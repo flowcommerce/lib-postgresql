88f7198a972562ac43bb9fa4492723734250ce53
Michael Bryzek <mbryzek@alum.mit.edu>
2015-11-21 13:27:41 -0500

    Expose prefix as a configurable parameter

--- app/io/flow/play/postgresql/AuditsDao.scala
+++ app/io/flow/play/postgresql/AuditsDao.scala
@@ -2,23 +2,29 @@ package io.flow.play.postgresql
 
 object AuditsDao {
 
-  def all(tableName: String) = {
-    val prefix = s"${tableName}_audit"
+  def all(
+    tableName: String,
+    prefix: Option[String] = None
+  ) = {
+    val p = prefix.getOrElse(tableName) + "_audit"
     Seq(
-      s"$tableName.created_at as ${prefix}_created_at",
-      s"$tableName.created_by_guid as ${prefix}_created_by_guid",
-      s"$tableName.updated_at as ${prefix}_updated_at",
-      s"$tableName.updated_by_guid as ${prefix}_updated_by_guid"
+      s"$tableName.created_at as ${p}_created_at",
+      s"$tableName.created_by_guid as ${p}_created_by_guid",
+      s"$tableName.updated_at as ${p}_updated_at",
+      s"$tableName.updated_by_guid as ${p}_updated_by_guid"
     ).mkString(", ")
   }
 
-  def creationOnly(tableName: String) = {
-    val prefix = s"${tableName}_audit"
+  def creationOnly(
+    tableName: String,
+    prefix: Option[String] = None
+  ) = {
+    val p = prefix.getOrElse(tableName) + "_audit"
     Seq(
-      s"$tableName.created_at as ${prefix}_created_at",
-      s"$tableName.created_by_guid as ${prefix}_created_by_guid",
-      s"$tableName.created_at as ${prefix}_updated_at",
-      s"$tableName.created_by_guid as ${prefix}_updated_by_guid"
+      s"$tableName.created_at as ${p}_created_at",
+      s"$tableName.created_by_guid as ${p}_created_by_guid",
+      s"$tableName.created_at as ${p}_updated_at",
+      s"$tableName.created_by_guid as ${p}_updated_by_guid"
     ).mkString(", ")
   }
 
