90ab26e0bcc2f780a0b913f172ee398bed47c272
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-21 10:51:32 -0800

    Add helper fromRow method

--- app/io/flow/play/postgresql/AuditsDao.scala
+++ app/io/flow/play/postgresql/AuditsDao.scala
@@ -1,5 +1,9 @@
 package io.flow.play.postgresql
 
+import io.flow.common.v0.models.{Audit, Reference}
+import org.joda.time.DateTime
+import java.util.UUID
+
 object AuditsDao {
 
   def all(
@@ -26,6 +30,24 @@ object AuditsDao {
       s"$tableName.created_at as ${p}_updated_at",
       s"$tableName.created_by_guid as ${p}_updated_by_guid"
     ).mkString(", ")
+  }
+
+  def fromRow(
+    row: anorm.Row,
+    prefix: Option[String] = None,
+    sep: Option[String] = Some("_")
+  ): Audit = {
+    val p = prefix.map( _ + sep.getOrElse("")).getOrElse("")
+    Audit(
+      createdAt = row[DateTime](s"${p}created_at"),
+      createdBy = Reference(
+        guid = row[UUID](s"${p}created_by_guid")
+      ),
+      updatedAt = row[DateTime](s"${p}updated_at"),
+      updatedBy = Reference(
+        guid = row[UUID](s"${p}updated_by_guid")
+      )
+    )
   }
 
 }
