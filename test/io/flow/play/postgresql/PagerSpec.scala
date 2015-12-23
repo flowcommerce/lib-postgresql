c8040f7ce1425af2d026766fbf8337734b9f1783
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-23 10:20:03 -0800

    Replace pager with one that implements iterator

---
+++ test/io/flow/play/postgresql/PagerSpec.scala
@@ -0,0 +1,46 @@
+package io.flow.play.postgresql
+
+import org.scalatest.{FunSpec, Matchers}
+
+class PagerSpec extends FunSpec with Matchers {
+
+  it("empty list") {
+    val pager = Pager.create { offset => Nil }
+    pager.hasNext should be(false)
+
+    intercept[NoSuchElementException] {
+      pager.next()
+    }
+  }
+
+  it("single page list") {
+    val pager = Pager.create { offset =>
+      offset match {
+        case 0 => Seq(1, 2)
+        case _ => Nil
+      }
+    }
+
+    pager.hasNext should be(true)
+    pager.next should be(1)
+    pager.hasNext should be(true)
+    pager.next should be(2)
+    pager.hasNext should be(false)
+  }
+
+  it("multiple pages") {
+    val pager = Pager.create { offset =>
+      offset match {
+        case 0 => Seq("a", "b")
+        case 2 => Seq("c")
+        case _ => Nil
+      }
+    }
+
+    pager.next should be("a")
+    pager.next should be("b")
+    pager.next should be("c")
+    pager.hasNext should be(false)
+  }
+
+}
