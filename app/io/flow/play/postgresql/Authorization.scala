2f1ddf34b6ba13ce241944030bd60abee9b1643f
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-26 13:49:43 -0800

    Add helper methods for Auth

--- app/io/flow/play/postgresql/Authorization.scala
+++ app/io/flow/play/postgresql/Authorization.scala
@@ -2,9 +2,27 @@ package io.flow.play.postgresql
 
 import java.util.UUID
 
-sealed trait Authorization {
+sealed trait Authorization
+
+object Authorization {
+
   case object PublicOnly extends Authorization
   case object All extends Authorization
   case class User(guid: UUID) extends Authorization
   case class Organization(key: String) extends Authorization
+
+  def fromUser(userGuid: Option[UUID]): Authorization = {
+    userGuid match {
+      case None => Authorization.PublicOnly
+      case Some(guid) => Authorization.User(guid)
+    }
+  }
+
+  def fromOrganization(orgId: Option[String]): Authorization = {
+    orgId match {
+      case None => Authorization.PublicOnly
+      case Some(id) => Authorization.Organization(id)
+    }
+  }
+  
 }
