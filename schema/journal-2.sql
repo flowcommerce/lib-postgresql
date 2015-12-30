33c557e648008200ede11ab00b41997aade9e75c
Michael Bryzek <mbryzek@alum.mit.edu>
2015-12-30 09:39:40 -0500

    Add index on primary key columns

--- schema/journal-2.sql
+++ schema/journal-2.sql
@@ -1,6 +1,3 @@
-create schema journal;
-set search_path to journal;
-
 create or replace function journal.refresh_journal_trigger(
   p_source_schema_name in varchar, p_source_table_name in varchar,
   p_target_schema_name in varchar, p_target_table_name in varchar
@@ -25,7 +22,6 @@ declare
   v_journal_name text;
   v_source_name text;
   v_trigger_name text;
-  v_first boolean;
   v_sql text;
 begin
   v_journal_name = p_target_schema_name || '.' || p_target_table_name;
@@ -34,21 +30,13 @@ begin
   -- create the function
   v_sql = 'create or replace function ' || v_journal_name || '_delete() returns trigger language plpgsql as ''';
   v_sql := v_sql || ' begin ';
-  v_sql := v_sql || '  insert into ' || v_journal_name || ' (';
+  v_sql := v_sql || '  insert into ' || v_journal_name || ' (journal_operation';
 
-  v_first = true;
   for row in (select column_name from information_schema.columns where table_schema = p_source_schema_name and table_name = p_source_table_name order by ordinal_position) loop
-
-    if (v_first) then
-      v_first := false;
-    else
-      v_sql := v_sql || ', ';
-    end if;
-    v_sql := v_sql || row.column_name;
-
+    v_sql := v_sql || ', ' || row.column_name;
   end loop;
 
-  v_sql := v_sql || ', journal_operation) values (old.*, TG_OP); ';
+  v_sql := v_sql || ') values (TG_OP, old.*); ';
   v_sql := v_sql || ' return null; end; ''';
 
   execute v_sql;
@@ -83,21 +71,13 @@ begin
   -- create the function
   v_sql = 'create or replace function ' || v_journal_name || '_insert() returns trigger language plpgsql as ''';
   v_sql := v_sql || ' begin ';
-  v_sql := v_sql || '  insert into ' || v_journal_name || ' (';
+  v_sql := v_sql || '  insert into ' || v_journal_name || ' (journal_operation';
 
-  v_first = true;
   for row in (select column_name from information_schema.columns where table_schema = p_source_schema_name and table_name = p_source_table_name order by ordinal_position) loop
-
-    if (v_first) then
-      v_first := false;
-    else
-      v_sql := v_sql || ', ';
-    end if;
-    v_sql := v_sql || row.column_name;
-
+    v_sql := v_sql || ', ' || row.column_name;
   end loop;
 
-  v_sql := v_sql || ', journal_operation) values (new.*, TG_OP); ';
+  v_sql := v_sql || ') values (TG_OP, new.*); ';
   v_sql := v_sql || ' return null; end; ''';
 
   execute v_sql;
@@ -123,6 +103,43 @@ begin
     when 'character' then p_column.data_type || '(' || p_column.character_maximum_length::varchar || ')'
     else p_column.data_type
     end;
+end;
+$$;
+
+create or replace function journal.create_primary_key_index(
+  p_source_schema_name in varchar, p_source_table_name in varchar,
+  p_target_schema_name in varchar, p_target_table_name in varchar
+) returns void language plpgsql as $$
+declare
+  row record;
+  v_columns character varying := '';
+begin
+  for row in (
+      select key_column_usage.column_name
+        from information_schema.table_constraints
+        join information_schema.key_column_usage
+             on key_column_usage.table_name = table_constraints.table_name
+            and key_column_usage.table_schema = table_constraints.table_schema
+            and key_column_usage.constraint_name = table_constraints.constraint_name
+       where table_constraints.constraint_type = 'PRIMARY KEY'
+         and table_constraints.table_schema = p_source_schema_name
+         and table_constraints.table_name = p_source_table_name
+       order by coalesce(key_column_usage.position_in_unique_constraint, 0),
+                coalesce(key_column_usage.ordinal_position, 0),
+                key_column_usage.column_name
+  ) loop
+
+    if v_columns != '' then
+      v_columns := v_columns || ', ';
+    end if;
+    v_columns := v_columns || row.column_name;
+
+  end loop;
+
+  if v_columns != '' then
+    execute 'create index on ' || p_target_schema_name || '.' || p_target_table_name || '(' || v_columns || ')';
+  end if;
+
 end;
 $$;
 
@@ -155,12 +172,11 @@ begin
     execute 'alter table ' || v_journal_name || ' add journal_operation text not null ';
     execute 'alter table ' || v_journal_name || ' add journal_id bigserial primary key ';
     execute 'comment on table ' || v_journal_name || ' is ''Created by plsql function refresh_journaling to shadow all inserts and updates on the table ' || p_source_schema_name || '.' || p_source_table_name || '''';
-    execute 'create index ' || p_target_table_name || '_id_idx on ' || v_journal_name || '(id)';
-
+    perform journal.create_primary_key_index(p_source_schema_name, p_source_table_name, p_target_schema_name, p_target_table_name);
   end if;
 
   perform journal.refresh_journal_trigger(p_source_schema_name, p_source_table_name, p_target_schema_name, p_target_table_name);
-
+  
   return v_journal_name;
 
 end;
