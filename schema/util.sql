7d9f1fdb61c7e12c6cfc3835c75311af56c86bcf
Michael Bryzek <mbryzek@alum.mit.edu>
2016-04-06 18:49:28 -0400

    Add upper utilities mimicing available lower functions

--- schema/util.sql
+++ schema/util.sql
@@ -36,6 +36,20 @@ create or replace function null_or_lower_non_empty_trimmed_string(p_value text) 
   end
 $$;
 
+create or replace function null_or_upper_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null then
+      return true;
+    else
+      if upper(trim(p_value)) = p_value and p_value != '' then
+        return true;
+      else
+        return false;
+      end if;
+    end if;
+  end
+$$;
+
 create or replace function null_or_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
   begin
     if p_value is null then
@@ -53,6 +67,16 @@ $$;
 create or replace function lower_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
   begin
     if (util.non_empty_trimmed_string(p_value) and lower(p_value) = p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function upper_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if (util.non_empty_trimmed_string(p_value) and upper(p_value) = p_value) then
       return true;
     else
       return false;
