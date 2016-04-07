1622f1e30dd46758f3c715b7a44b4e868679ee1f
Michael Bryzek <mbryzek@alum.mit.edu>
2016-04-07 19:00:12 -0400

    Add utilities for upper, country, currency, language to help ensure consistency (#21)
    * Add upper utilities mimicing available lower functions
    
    * Add specific country/currency/language functions to help ensure consistency

--- schema/util.sql
+++ schema/util.sql
@@ -36,6 +36,20 @@ create or replace function null_or_lower_non_empty_trimmed_string(p_value text) 
   end
 $$;
 
+create or replace function null_or_upper_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null then
+      return true;
+    else
+      if upper(trim(p_value)) = p_value and p_value != '' then
+        return true;
+      else
+        return false;
+      end if;
+    end if;
+  end
+$$;
+
 create or replace function null_or_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
   begin
     if p_value is null then
@@ -53,6 +67,76 @@ $$;
 create or replace function lower_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
   begin
     if (util.non_empty_trimmed_string(p_value) and lower(p_value) = p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function upper_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if (util.non_empty_trimmed_string(p_value) and upper(p_value) = p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function null_or_currency_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or currency_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function currency_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if upper(trim(p_value)) = p_value and length(p_value) = 3 then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function null_or_country_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or country_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function country_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if upper(trim(p_value)) = p_value and length(p_value) = 3 then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function null_or_language_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or language_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function language_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if lower(trim(p_value)) = p_value and length(p_value) = 2 then
       return true;
     else
       return false;
