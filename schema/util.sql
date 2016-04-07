edb840b5c3b45c54f283ba7dbc4e21583ca33972
Michael Bryzek <mbryzek@alum.mit.edu>
2016-04-06 21:13:48 -0400

    Add specific country/currency/language functions to help ensure consistency

--- schema/util.sql
+++ schema/util.sql
@@ -83,3 +83,63 @@ create or replace function upper_non_empty_trimmed_string(p_value text) returns 
     end if;
   end
 $$;
+
+create or replace function null_or_currency_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or currency_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function currency_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if upper(trim(p_value)) = p_value and length(p_value) = 3 then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function null_or_country_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or country_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function country_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if upper(trim(p_value)) = p_value and length(p_value) = 3 then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function null_or_language_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null or language_code(p_value) then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
+
+create or replace function language_code(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if lower(trim(p_value)) = p_value and length(p_value) = 2 then
+      return true;
+    else
+      return false;
+    end if;
+  end
+$$;
