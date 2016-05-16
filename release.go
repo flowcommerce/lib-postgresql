b476d25d7ad2ac123fcb7c8e057d3e7d30b46343
mikeclimbrock <mikeroth@flow.io>
2016-05-16 17:51:31 -0400

    add release.go script

---
+++ release.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/flowcommerce/tools/executor"
+)
+
+func main() {
+	executor := executor.Create("lib-postgres")
+	executor = executor.Add("dev tag")
+	executor = executor.Add("sbt publish")
+	executor.Run()
+}
