e0c683d56b311ff30fc64ea7abd322475144ad54
Michael Bryzek <mbryzek@alum.mit.edu>
2016-08-23 14:41:03 -0400

    Set fill factor to 100 for journals (#39)
    * Fix typo
    
    * Set fill factor to 100 for journals

--- schema/journal-2.sql
+++ schema/journal-2.sql
@@ -222,6 +222,7 @@ begin
     execute 'alter table ' || v_journal_name || ' add journal_timestamp timestamp with time zone not null default now() ';
     execute 'alter table ' || v_journal_name || ' add journal_operation text not null ';
     execute 'alter table ' || v_journal_name || ' add journal_id bigserial primary key ';
+    execute 'alter table ' || v_journal_name || ' set (fillfactor=100) ';
     execute 'comment on table ' || v_journal_name || ' is ''Created by plsql function refresh_journaling to shadow all inserts and updates on the table ' || p_source_schema_name || '.' || p_source_table_name || '''';
     perform journal.add_primary_key_data(p_source_schema_name, p_source_table_name, p_target_schema_name, p_target_table_name);
     perform journal.create_prevent_update_trigger(p_target_schema_name, p_target_table_name);
