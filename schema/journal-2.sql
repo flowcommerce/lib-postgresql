c8016d57cbf6f8a68083eaa6f780e7210e6c1a26
Paolo Lim <paolo@flow.io>
2016-09-09 09:27:05 -0400

    Fix numeric type in journal.get_data_type_string (#41)

--- schema/journal-2.sql
+++ schema/journal-2.sql
@@ -132,14 +132,22 @@ $$;
 create or replace function journal.get_data_type_string(
   p_column information_schema.columns
 ) returns varchar language plpgsql as $$
+declare
+  journal_data_type   text;
 begin
-  return case p_column.data_type
-    when 'numeric' then p_column.data_type || '(' || p_column.numeric_precision_radix::varchar || ',' || p_column.numeric_scale::varchar || ')'
-    when 'character' then 'text'
-    when 'character varying' then 'text'
-    when '"char"' then 'text'
-    else p_column.data_type
-    end;
+  IF p_column.data_type = 'numeric' THEN
+    IF p_column.numeric_precision IS NOT NULL AND p_column.numeric_scale IS NOT NULL THEN
+      journal_data_type := p_column.data_type || '(' || p_column.numeric_precision::varchar || ',' || p_column.numeric_scale::varchar || ')';
+    ELSE
+      journal_data_type := p_column.data_type;
+    END IF;
+  ELSEIF p_column.data_type IN ('character', 'character varying', '"char"') THEN
+    journal_data_type := 'text';
+  ELSE
+    journal_data_type := p_column.data_type;
+  END IF;
+
+  return journal_data_type;
 end;
 $$;
 
