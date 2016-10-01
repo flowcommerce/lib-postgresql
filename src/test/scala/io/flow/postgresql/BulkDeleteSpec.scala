b77f7f465628b7c68beb4d4e021ba333a99804f2
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-01 18:13:00 -0400

    Better test

--- src/test/scala/io/flow/postgresql/BulkDeleteSpec.scala
+++ src/test/scala/io/flow/postgresql/BulkDeleteSpec.scala
@@ -4,43 +4,43 @@ import org.scalatest.{FunSpec, Matchers}
 
 class BulkDeleteSpec extends FunSpec with Matchers {
 
-  def deleteFunction(item: String): String = {
-    println(s"Element [$item] deleted")
-    item
-  }
-
   it("empty list") {
     val emptyList = Seq()
 
     BulkDelete.byPage {
       emptyList
     } { e =>
-      deleteFunction(e)
+      sys.error("Should not have iterator")
     }
   }
 
   it("list with single element") {
-    var listWithOneElement = Seq("x")
-    BulkDelete.byPage {
-      listWithOneElement
-    } { e =>
-      val d = deleteFunction(e)
+    val original = Seq("x")
+    var items = original
+    var found = scala.collection.mutable.ListBuffer[String]()
 
-      if(d == "x")
-        listWithOneElement = Seq()
+    BulkDelete.byPage {
+      items
+    } { e =>
+      found += e
+      items = items.filter(_ != e)
     }
+    found should equal(original)
   }
 
   it("list with multiple elements") {
-    var listWithMultiElement = Seq("a", "b", "c")
-    BulkDelete.byPage {
-      listWithMultiElement
-    } { e =>
-      val d = deleteFunction(e)
+    val original = Seq(1, 2, 3)
+    var items = original
+    var found = scala.collection.mutable.ListBuffer[Int]()
 
-      if(d == "c")
-        listWithMultiElement = Seq()
+    BulkDelete.byPage {
+      println("ADSFASDF")
+      items
+    } { e =>
+      found += e
+      items = items.filter(_ != e)
     }
+    found should equal(original)
   }
 
 }
