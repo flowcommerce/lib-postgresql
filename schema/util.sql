41bc25d0dc6051af00e45846e2a10a450ebf12f9
mikeclimbrock <roth@flow.io>
2017-03-14 14:07:17 -0400

    accept null or trimmed empty/non-empty string

--- schema/util.sql
+++ schema/util.sql
@@ -64,6 +64,20 @@ create or replace function null_or_non_empty_trimmed_string(p_value text) return
   end
 $$;
 
+create or replace function null_or_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
+  begin
+    if p_value is null then
+      return true;
+    else
+      if trim(p_value) = p_value then
+        return true;
+      else
+        return false;
+      end if;
+    end if;
+  end
+$$;
+
 create or replace function lower_non_empty_trimmed_string(p_value text) returns boolean immutable cost 1 language plpgsql as $$
   begin
     if (util.non_empty_trimmed_string(p_value) and lower(p_value) = p_value) then
