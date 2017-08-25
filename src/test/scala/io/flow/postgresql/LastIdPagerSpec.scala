2f7fc6f48f41f8e86665c619527db1973c8ec4bb
Jean Logeart <jean.logeart@gmail.com>
2017-08-24 20:19:03 -0400

    Add a last id pager

---
+++ src/test/scala/io/flow/postgresql/LastIdPagerSpec.scala
@@ -0,0 +1,51 @@
+package io.flow.postgresql
+
+import org.scalatest.{FunSpec, Matchers}
+
+class LastIdPagerSpec extends FunSpec with Matchers {
+
+  it("empty list") {
+    val pager = Pager.byLastId[Long, Long](lastId => Nil, identity)
+    pager.hasNext should be(false)
+
+    intercept[NoSuchElementException] {
+      pager.next()
+    }
+  }
+
+  it("single page list") {
+    val pager = Pager.byLastId[String, Long](
+      {
+        case None => Seq("*", "**")
+        case Some(2L) => Nil
+      },
+      _.size
+    )
+
+    pager.hasNext should be(true)
+    pager.next should be("*")
+    pager.hasNext should be(true)
+    pager.next should be("**")
+    pager.hasNext should be(false)
+  }
+
+  it("multiple pages") {
+    val pager = Pager.byLastId[String, Long](
+      {
+        case None => Seq("*", "**")
+        case Some(2L) => Seq("****")
+        case Some(4L) => Nil
+      },
+      _.size
+    )
+
+    pager.hasNext should be(true)
+    pager.next should be("*")
+    pager.hasNext should be(true)
+    pager.next should be("**")
+    pager.hasNext should be(true)
+    pager.next should be("****")
+    pager.hasNext should be(false)
+  }
+
+}
