e2cd50bb1a7ad5380bb6806d6690807f01c11590
Michael Bryzek <mike@flow.io>
2019-11-02 17:39:59 -0400

    update

--- src/test/scala/io/flow/postgresql/QueryBatchSpec.scala
+++ src/test/scala/io/flow/postgresql/QueryBatchSpec.scala
@@ -2,10 +2,12 @@ package io.flow.postgresql
 
 import java.util.UUID
 
+import anorm.SqlParser
 import io.flow.postgresql.db.TestDatabase
 import org.scalatest.{FunSpec, Matchers}
 
 class QueryBatchSpec extends FunSpec with Matchers {
+  private[this] val SelectUserQuery = Query("select name from test_users")
 
   private[this] val UpsertUserQuery = Query(
     """
@@ -19,21 +21,36 @@ class QueryBatchSpec extends FunSpec with Matchers {
   )
   case class User(id: String, name: String)
 
+  def findUserById(id: String): Option[User] = {
+    TestDatabase.withConnection { implicit c =>
+      SelectUserQuery
+        .equals("id", id)
+        .as(SqlParser.str("name").*)
+        .headOption.map { n =>
+        User(id = id, name = n)
+      }
+    }
+  }
+
   def createUser(name: String): User = {
     val id = UUID.randomUUID().toString
     TestDatabase.withConnection { implicit c =>
-      UpsertUserQuery.withDebugging()
+      UpsertUserQuery
         .bind("id", id)
         .bind("name", name)
         .anormSql().execute()
     }
-    User(
-      id = id,
-      name = name,
-    )
+    findUserById(id).get
   }
 
-  it("batch_upsert") {
+  it("create single user") {
+    val user1 = createUser(name = "A")
+    val user2 = createUser(name = "B")
+    findUserById(user1.id).get.name should be("A")
+    findUserById(user2.id).get.name should be("B")
+  }
+
+  it("create users in batch") {
     val user1 = createUser(name = "A")
     val user2 = createUser(name = "B")
     println(s"user[${user1}] --  $user2")
