3351d299d637afb04de81d028b83cea6302c3519
Michael Bryzek <mike@flow.io>
2019-12-03 08:17:51 -0500

    Sanitize: Expose sanitize method to enable checking if input is safe (#153)

---
+++ src/main/scala/io/flow/postgresql/Sanitize.scala
@@ -0,0 +1,15 @@
+package io.flow.postgresql
+
+object Sanitize {
+
+  val ValidCharacters: Set[String] = "_-,.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("").toSet
+
+  /**
+    * Returns true IFF the string contains only safe
+    * characters
+    */
+  def isSafe(value: String): Boolean = {
+    value.trim.split("").forall(ValidCharacters.contains)
+  }
+
+}
