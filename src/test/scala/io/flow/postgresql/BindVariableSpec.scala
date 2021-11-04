bb5d8c459e538c6652fdef4aa9b4c7b18bc54132
Gregor Heine <gregor@flow.io>
2021-11-04 22:55:14 +0000

    more tests

--- src/test/scala/io/flow/postgresql/BindVariableSpec.scala
+++ src/test/scala/io/flow/postgresql/BindVariableSpec.scala
@@ -7,6 +7,7 @@ class BindVariableSpec extends AnyFunSpec with Matchers {
 
   it("safeName") {
     BindVariable.safeName("user") should be("user")
+    BindVariable.safeName(" user ") should be("user")
     BindVariable.safeName("_first") should be("first")
     BindVariable.safeName("first_") should be("first")
     BindVariable.safeName("_first_") should be("first")
@@ -17,6 +18,7 @@ class BindVariableSpec extends AnyFunSpec with Matchers {
     BindVariable.safeName("users._first") should be("first")
     BindVariable.safeName("users._first_") should be("first")
     BindVariable.safeName("_users.first") should be("first")
+    BindVariable.safeName(" users. first ") should be("first")
 
     BindVariable.safeName("users.first_name") should be("first_name")
     BindVariable.safeName("users.first__name") should be("first_name")
@@ -26,6 +28,10 @@ class BindVariableSpec extends AnyFunSpec with Matchers {
     BindVariable.safeName("UsErS.FiRsT") should be("first")
     BindVariable.safeName("üšéRŸ") should be("r")
     BindVariable.safeName("üUšSéEęRŸ") should be("u_s_e_r")
+
+    BindVariable.safeName("0123456789") should be("0123456789")
+    BindVariable.safeName("abcdefghijklmnopqrstuvwxyz") should be("abcdefghijklmnopqrstuvwxyz")
+    BindVariable.safeName("ABCDEFGHIJKLMNOPQRSTUVWXYZ") should be("abcdefghijklmnopqrstuvwxyz")
   }
 
   it("safeName is never empty") {
@@ -34,6 +40,7 @@ class BindVariableSpec extends AnyFunSpec with Matchers {
     BindVariable.safeName("_") should be("bind")
     BindVariable.safeName("   ") should be("bind")
     BindVariable.safeName("____") should be("bind")
+    BindVariable.safeName("üšéŸß") should be("bind")
   }
   
 }
